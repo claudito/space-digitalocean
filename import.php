56f8d87700071d4541ef91621b1e5381a59a62d3
Luis Augusto Claudio Ponce <1107000044@upci.edu.pe>
2020-01-25 18:11:29 -0500

    Actualización

--- import.php
+++ import.php
@@ -1,49 +1,55 @@
 <?php 
 
-include'vendor/autoload.php';
+include '../vendor/autoload.php';
 
 use Aws\S3\S3Client;
 use League\Flysystem\AwsS3v3\AwsS3Adapter;
 use League\Flysystem\Filesystem;
 
 
+$filename    =  $_FILES['archivo']['name']; //Nombre del Archivo
+$extension   =  pathinfo($_FILES['archivo']['name'],PATHINFO_EXTENSION);//Extensión del Archivo
+$filename    =  "nuevo_nombre".$extension;//Nombre del Archivo con nuevo nombre
+
+$destination =  $_FILES['archivo']['tmp_name'];//Ruta Temporal
+$endpoint    =  "https://fra1.digitaloceanspaces.com";//End Point: Considerar el prefijp https://
+$folder      =  "imagenes/"; //Folder que se creara en dentro del Space
+$space       =  "spacecloud"; //Nombre del Space
+$permiso     =  "public-read"; //Permiso de Lectura Habilitado
+
 try {
-
-
-$nombre    =  $_FILES['archivo']['name']; //nombre del archivo
-$ubicacion =  $_FILES['archivo']['tmp_name']; //ruta temporal del archivo
-
+	
+//Configuracíón
 $client = new S3Client([
     'credentials' => [
-        'key'    => 'KEY',//Generado en el administrador de los Spaces
-        'secret' => 'SECRET', //Generado en el administrador de los Spaces
+        'key'    => 'your-key',//key
+        'secret' => 'your-secret',//secret
     ],
-    'region'   => 'nyc3', //Region donde se crea el space
-    'version'  => 'latest',//Significa que estamos utilizamos la última versión
-    'endpoint' => 'https://perutec.nyc3.digitaloceanspaces.com', //Nombre del space que le colocamos al momnento de crearlo
+    'region' 	=> 'fra1',//Region
+    'version' 	=> 'latest',//version
+    'endpoint' 	=> $endpoint, //endpoint
 ]);
 
-$adapter = new AwsS3Adapter($client, 'nombre.carpeta');//Nombre de la carpeta donde vamos a subir nuestro archivo(El nombre del archivo debe llevar un punto)
+//Subimo el archivo
+ $result = $client->putObject([
 
-$filesystem = new Filesystem($adapter);
+            'Bucket'     => $space,//space
+            'Key'        => $folder.$filename, //Ubicación del Archivo en tu Space
+            'SourceFile' => $destination, //ruta temporal del archivo
+             'ACL'       => $permiso // indicamos que se subira en modo publico, si quitamos en esta linea se subira en modo privado
+       
 
-
-  $result = $client->putObject([
-
-            'Bucket'     => 'nombre.carpeta', //Carpeta
-            'Key'        => $nombre, //nombre del archivo
-            'SourceFile' => $ubicacion, //ruta temporal del archivo
-             'ACL'       => 'public-read' // indicamos que se subira en modo publico, si quitamos en esta linea se subira en modo privado
 
         ]);
 
+
+ echo $result;
 
 
 
 } catch (Exception $e) {
 
 echo "Error: ".$e->getMessage();
-
 	
 }
 
